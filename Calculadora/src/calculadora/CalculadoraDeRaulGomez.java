0e51a8398ba9a5accb41d4f7b840d821fd215e70
The-Rul <raaulgs@gmail.com>
2023-01-15 23:31:29 +0100

    Segundo intento commit en remoto

--- Calculadora/src/calculadora/CalculadoraDeRaulGomez.java
+++ Calculadora/src/calculadora/CalculadoraDeRaulGomez.java
@@ -30,6 +30,7 @@ public class CalculadoraDeRaulGomez {
 	public int divide(int num1, int num2) {
 		int resul = num1 / num2;
 	System.out.println("Hola");
+	System.out.println("mirame");
 		return resul;
 	}
 	
